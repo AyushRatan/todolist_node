6681d70868a425a2ee4cf12296344d61bac21000
AyushRatan <ayushratan.974@gmail.com>
2023-05-03 20:22:27 +0530

    integrate with mongo

--- app.js
+++ app.js
@@ -1,41 +1,147 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 const day = require(__dirname+"/date.js");
 const app = express();
+require("dotenv").config();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
-const checklist = ["Search game","Buy game","Play game"];
-const worklist =[];
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.am4sgae.mongodb.net/todolist`);
+
+const todolistSchema = new mongoose.Schema({
+    name:String
+});
+
+const Item = mongoose.model("Item",todolistSchema);
+
+const item1 = new Item({
+    name:"Welcome todolist"
+});
+const item2 = new Item({
+    name:"click '+' to add new items"
+});
+const item3 = new Item({
+    name:"<-- click here to delete items"
+});
+
+const listSchema = new mongoose.Schema({
+    name:String,
+    items: [todolistSchema]
+})
+
+const List = mongoose.model("List",listSchema);
+
+const defaultitems = [item1,item2,item3];
+// Item.insertMany([item1,item2,item3]).then(
+//     ()=>{
+//         console.log("insert success");
+//     }
+// )
+
+// const checklist = ["Search game","Buy game","Play game"];
+// const worklist =[];
 
 
 app.get("/",function(req,res){
     
-    const dayname = day.getDay();
-    res.render("list",{Listtitle:dayname,checklist:checklist});
+    // const dayname = day.getDay();
+    Item.find({}).then(
+        function(items){
+            if(items.length==0){
+                Item.insertMany(defaultitems).then(
+                    ()=>"Default items inserted"
+                )
+                res.redirect("/")
+            }else{
+                res.render("list",{Listtitle:"Today",checklist:items});
+            }
+        }
+    ).catch(function(err){
+        console.log(err)
+    })
+    
+    
 });
 
+app.get("/:customListName",function(req,res){
+    const customListName = _.capitalize(req.params.customListName);
+    List.findOne({name:customListName}).then(
+        function(foundlist){
+            if(!foundlist){
+                const list = new List({
+                    name:customListName,
+                    items:defaultitems
+                })
+                list.save();
+                res.redirect("/"+customListName);
+            }else{
+                res.render("list",{Listtitle:customListName,checklist:foundlist.items});
+            }
+        }
+    ).catch(function(err){
+        console.log(err);
+    })
+    
+    // list.save();
+});
+
+
+
 app.post("/",function(req,res){
-    console.log(req.body);
-    if(req.body.list_type === "Work"){
-        worklist.push(req.body.item);
-        res.redirect("/work")
-    }else{
-        checklist.push(req.body.item);
+    const itemname = req.body.item;
+    const listname = req.body.list_type;
+
+    const new_item = new Item({
+        name:itemname
+    })
+
+    if(listname==="Today"){
+        new_item.save();
         res.redirect("/");
+
+    }else{
+        List.findOne({name:listname}).then(
+            function(foundlist){
+                foundlist.items.push(new_item);
+                foundlist.save();
+                res.redirect("/"+listname);
+            }
+        )
     }
 
 });
 
-app.get("/work",function(req,res){
-    res.render("list",{Listtitle:"Work",checklist:worklist})
-});
+app.post("/delete",function(req,res){
+    const checkbox_id = req.body.checkbox;
+    const listname = req.body.listname;
+    console.log(checkbox_id,listname);
 
-app.post("/work",function(req,res){
-    const item = req.body.item;
-    worklist.push(item);
-    res.redirect("/work");
-});
+    if(listname==="Today"){
+        Item.deleteOne({_id:checkbox_id}).then(
+            ()=>{
+                res.redirect("/")
+            }
+        )
+    }else{
+        List.findOneAndUpdate({name:listname},{$pull:{items:{_id:checkbox_id}}}).then(
+            ()=>{
+                res.redirect("/"+listname)
+            }
+        ).catch((err)=>{console.log(err)});
+    }
+    
+})
+
+
+
+// app.post("/work",function(req,res){
+//     const item = req.body.item;
+//     worklist.push(item);
+//     res.redirect("/work");
+// });
 
 
 
